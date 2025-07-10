0ef5d3487b3b9e581ce42ee4bc0b3ef05d7e4585
Hongfeng Xia <nemo_xia@yahoo.com.au>
2025-07-10 17:19:13 +0800

    Update main.py
    print target_host

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import os
 import argparse
 
 apikey = os.getenv("MAXIMO_APIKEY")
+target_host = os.getenv("TARGETHOST")
 parser = argparse.ArgumentParser(description="Process folder and product")
 parser.add_argument("folder", help="Path to devops autoscript folder")
 parser.add_argument("product", help="Name of the product")
 
 args = parser.parse_args()
 
-print(apikey, args.folder, args.product)
+print(target_host, apikey, args.folder, args.product)
